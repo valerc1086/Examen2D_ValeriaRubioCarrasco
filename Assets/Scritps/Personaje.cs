dfb41dd1b0f489b393e79b254c6eb4b060e6da69
Valeria <valerc1096@gmail.com>
2025-11-19 17:41:37 +0100

    Tercer commit del examen

---
+++ Assets/Scritps/Personaje.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Personaje : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float velocidad = 2;
+    private float movimientoX;
+    public Rigidbody2D rb2d;
+
+    [Header("Salto")]
+    public float fuerzaSalto = 2;
+    private bool estaEnSuelo;
+    public LayerMask layerSuelo;
+    public float radioEsferaTocaSuelo = 0.2f;
+    public Transform compruebaSuelo;
+
+    [Header("Animaciones")]
+    Animator animator;
+
+    [Header("Sonido")]
+    public AudioSource audioSource;
+    public AudioClip getMoneda;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+    }
+
+    
+    void Update()
+    {
+        rb2d.linearVelocity = new Vector2(movimientoX * velocidad, rb2d.linearVelocity.y);
+        if(movimientoX == 0)
+        {
+            //animator.SetBool();
+        }
+    }
+
+    void FixedUpdate() { 
+    
+        estaEnSuelo = Physics2D.OverlapCircle(compruebaSuelo.position, radioEsferaTocaSuelo, layerSuelo);
+    }
+
+    private void OnMove(InputValue inputMovimiento)
+    {
+        movimientoX = inputMovimiento.Get<Vector2>().x;
+        //animator.SetBool();
+
+        if(movimientoX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(movimientoX), 1, 1);
+        }
+    }
+
+    private void OnJump(InputValue inputSalto)
+    {
+        if (estaEnSuelo)
+        {
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, fuerzaSalto);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Enemigo"))
+        {
+            StartCoroutine(ReiniciarNivel());
+        }
+        if (collision.transform.CompareTag("SueloMorir"))
+        {
+            StartCoroutine(ReiniciarNivel());
+        }
+        if (collision.transform.CompareTag("Moneda"))
+        {
+            Destroy(collision.gameObject);
+            Fin
+        }
+    }
+
+    IEnumerator ReiniciarNivel()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
